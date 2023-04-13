6c5efd5648cc912cc573f096ad24d8ddd737d9c0
Andrey Arzumaniyan <88486829+AndrewArzumaniyan@users.noreply.github.com>
2023-04-14 00:53:01 +0600

    update solution

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import mannwhitneyu
 
 
 chat_id = 938988157 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.03
+    pvalue = mannwhitneyu(x, y, alternative=alternative).pvalue
+    
+    return pvalue < alpha 
