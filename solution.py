6c90ac20b4d6b1fe16c04aebf553fa64a0dd9b18
Andrey Arzumaniyan <88486829+AndrewArzumaniyan@users.noreply.github.com>
2023-04-14 00:57:55 +0600

    update

--- solution.py
+++ solution.py
@@ -7,6 +7,6 @@ chat_id = 938988157 # Ваш chat ID, не меняйте название пе�
 
 def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     alpha = 0.03
-    pvalue = mannwhitneyu(x, y, alternative=alternative).pvalue
+    pvalue = mannwhitneyu(x, y, alternative="less").pvalue
     
     return pvalue < alpha 
